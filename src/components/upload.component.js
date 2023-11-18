c99d53f2a8321d73fd5fe5b49ce50469e3ba7b56
ShashwotBhattarai <shashwot.tech@gmail.com>
2023-11-19 03:33:34 +0545

    changed console log

--- src/components/upload.component.js
+++ src/components/upload.component.js
@@ -41,7 +41,7 @@ const Upload = () => {
         }
       );
 
-      console.log(response);
+      console.log(response.data);
     } catch (error) {
       console.error("Error sending data to the backend:", error.message);
     }
