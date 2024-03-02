6981b2662c1bd5c10e9e3ebf3e62cc1d48937aeb
shashwotbhattarai <shashwot.tech@gmail.com>
2024-03-02 18:07:07 +0545

    added error handling for failed signup

--- src/services/signUp.service.js
+++ src/services/signUp.service.js
@@ -1,6 +1,5 @@
 export async function SignUp(formData) {
   try {
-    console.log("formdata", formData);
     const response = await fetch(`http://localhost:3001/auth/signup`, {
       method: "POST",
       headers: {
@@ -10,7 +9,6 @@ export async function SignUp(formData) {
       },
       body: JSON.stringify({ email: formData.email, role: formData.role }),
     });
-    console.log("response", response);
     if (response.status === 201) {
       return {
         status: 200,
@@ -18,20 +16,17 @@ export async function SignUp(formData) {
         data: null,
       };
     } else {
-      const err = new Error("Error: " + response.message);
-      err.status = response.status;
-
-      err.data = null;
-
-      throw err;
+      return {
+        status: response.status,
+        message: response.message,
+        data: null,
+      };
     }
   } catch (error) {
-    const err = new Error(
-      "Unknown error in SignUp.Please see inisde data for more details",
-    );
-    err.status = 500;
-    err.data = error;
-
-    throw err;
+    return {
+      status: 500,
+      message: error.message,
+      data: null,
+    };
   }
 }
